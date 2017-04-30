65ac2e8daf4d4128daca7a0c1d1016bca4255d8e
John Biddiscombe <biddisco@cscs.ch>
2017-05-01 00:25:53 +0200

    Adding rma namespace and adding support for rma_object and is_rma_eligible traits
    Moving lots of rma memory registration code into the rma namespace and
    out of the parcelport subdirectories.
    
    An rma_object is any type that has a contiguous memory layout and can in
    principle be a source or destination of rma read/write type operations.
    
    is_rma_eligible is a trait that is used to enable rma creation of a
    type.
    
    rma_object provides a wrapper (like shared_ptr) around a T, creation
    of an rma_object must be via the make_rma_object(...) function which
    will use the rma allocator provided by the parcelport layer (currently
    the default parcelport, though this will be extended to take a locality
    ID to ensure that the correct parcelport is used for a given
    destination).
    
    Currently rma_object supports only simple T types, vectors of T and
    arrays of T.
    
    Provide allocator support for STL (vector) using
    rma_object<std::vector<T, rma_allocator<T>>>

--- plugins/parcelport/libfabric/parcelport_libfabric.cpp
+++ plugins/parcelport/libfabric/parcelport_libfabric.cpp
@@ -39,7 +39,6 @@
 // when we have maxed out the number of sends we can handle
 #define HPX_PARCELPORT_LIBFABRIC_SUSPEND_WAKE  (HPX_PARCELPORT_LIBFABRIC_THROTTLE_SENDS/2)
 
-
 // --------------------------------------------------------------------
 // Enable the use of boost small_vector for certain short lived storage
 // elements within the parcelport. This can reduce some memory allocations
@@ -51,15 +50,16 @@
 #define HPX_PARCELPORT_LIBFABRIC_IMM_UNSUPPORTED 1
 
 // --------------------------------------------------------------------
+#include <hpx/runtime/parcelset/rma/memory_pool.hpp>
+//
+#include <plugins/parcelport/parcelport_logging.hpp>
+#include <plugins/parcelport/performance_counter.hpp>
 #include <plugins/parcelport/unordered_map.hpp>
+//
 #include <plugins/parcelport/libfabric/header.hpp>
 #include <plugins/parcelport/libfabric/locality.hpp>
-
 #include <plugins/parcelport/libfabric/libfabric_region_provider.hpp>
-#include <plugins/parcelport/performance_counter.hpp>
-#include <plugins/parcelport/rma_memory_pool.hpp>
 #include <plugins/parcelport/libfabric/connection_handler.hpp>
-#include <plugins/parcelport/parcelport_logging.hpp>
 #include <plugins/parcelport/libfabric/rdma_locks.hpp>
 #include <plugins/parcelport/libfabric/libfabric_controller.hpp>
 
@@ -169,6 +169,11 @@ namespace libfabric
         LOG_DEBUG_MSG("Fetching memory pool");
         chunk_pool_ = &libfabric_controller_->get_memory_pool();
 
+        // setup provider specific allocator for rma_object use
+        rma::detail::rma_allocator_impl<char, libfabric_region_provider> *default_allocator =
+            new rma::detail::rma_allocator_impl<char, libfabric_region_provider>(chunk_pool_);
+        allocator_ = default_allocator;
+        //
         for (std::size_t i = 0; i < HPX_PARCELPORT_LIBFABRIC_THROTTLE_SENDS; ++i)
         {
             sender *snd =
@@ -266,8 +271,8 @@ namespace libfabric
             delete snd;
         }
         LOG_DEBUG_MSG(
-            << "sends_posted "  << decnumber(sends_posted)
-            << "sends_deleted " << decnumber(sends_posted)
+               "sends_posted "  << decnumber(sends_posted)
+            << "sends_deleted " << decnumber(sends_deleted)
             << "acks_received " << decnumber(acks_received)
             << "non_rma-send "  << decnumber(sends_posted-acks_received));
         //
--- plugins/parcelport/libfabric/sender.hpp
+++ plugins/parcelport/libfabric/sender.hpp
@@ -7,21 +7,22 @@
 #ifndef HPX_PARCELSET_POLICIES_LIBFABRIC_SENDER_HPP
 #define HPX_PARCELSET_POLICIES_LIBFABRIC_SENDER_HPP
 
-#include <plugins/parcelport/libfabric/libfabric_region_provider.hpp>
+#include <hpx/runtime/parcelset/rma/detail/memory_region_impl.hpp>
+#include <hpx/runtime/parcelset/rma/memory_pool.hpp>
+#include <hpx/runtime/parcelset/locality.hpp>
+#include <hpx/util/atomic_count.hpp>
+#include <hpx/util/unique_function.hpp>
+//
 #include <plugins/parcelport/performance_counter.hpp>
-#include <plugins/parcelport/rma_memory_pool.hpp>
+//
+#include <plugins/parcelport/libfabric/libfabric_region_provider.hpp>
 #include <plugins/parcelport/libfabric/pinned_memory_vector.hpp>
 #include <plugins/parcelport/libfabric/header.hpp>
 #include <plugins/parcelport/libfabric/rma_base.hpp>
-
-#include <hpx/runtime/parcelset/locality.hpp>
-
-#include <hpx/util/atomic_count.hpp>
-#include <hpx/util/unique_function.hpp>
-
+//
 #include <boost/container/small_vector.hpp>
+//
 #include <memory>
-
 // include for iovec
 #include <sys/uio.h>
 
@@ -34,9 +35,9 @@ namespace libfabric
 
     struct sender : public rma_base
     {
-        typedef libfabric_region_provider                region_provider;
-        typedef rma_memory_region<region_provider>       region_type;
-        typedef rma_memory_pool<region_provider>         memory_pool_type;
+        typedef libfabric_region_provider                        region_provider;
+        typedef rma::detail::memory_region_impl<region_provider> region_type;
+        typedef rma::memory_pool<region_provider>                memory_pool_type;
 
         typedef header<HPX_PARCELPORT_LIBFABRIC_MESSAGE_HEADER_SIZE> header_type;
         static constexpr unsigned int header_size = header_type::header_block_size;
